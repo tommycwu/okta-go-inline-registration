70a3b250a58c4f2d17cc47661f6c853145eed11c
tommycwu <31978871+tommycwu@users.noreply.github.com>
2020-12-08 21:27:13 -0800

    Update registration.go

--- registration.go
+++ registration.go
@@ -7,7 +7,7 @@ import (
 )
 
 func reqHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var body = `{"commands": [{"type":"com.okta.action.update","value":[{"registration": "DENY"}]}]}`
+	var body = ``
 	resp := events.APIGatewayProxyResponse{
 		StatusCode:      200,
 		IsBase64Encoded: false,
