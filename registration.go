fb4297d299954a8b6a747989d322a16e11be6b5b
tommycwu <31978871+tommycwu@users.noreply.github.com>
2020-12-09 00:10:21 -0800

    Update registration.go

--- registration.go
+++ registration.go
@@ -24,12 +24,7 @@ func reqHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRe
 		}
 	}`
 	var bodystring = request.Body
-	if strings.Contains(bodystring, "@mailinator.com"){
-		log.Println("yes")
-        } else {
-		log.Println("no")
-	}
-	log.Println(request.Body)
+	log.Println(bodystring)
 
 	resp := events.APIGatewayProxyResponse{
 		StatusCode:      200,
